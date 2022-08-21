d3600b35bc390602521625a53422bcf3c0b13cf3
abhixshakya <abhixshakya@gmail.com>
2022-08-22 01:37:41 +0530

    final commit

--- src/components/Categories/categories_data.js
+++ src/components/Categories/categories_data.js
@@ -33,83 +33,80 @@ export const catData = [
   //   title: "Business",
   //   img_path: require("../images/category_img/Business.png"),
   // },
-  {
-    title: "Leadership",
-    img_path: require("../images/category_img/Leadership.png"),
-  },
-  {
-    title: "Sales",
-    img_path: require("../images/category_img/Sales.png"),
-  },
+  // {
+  //   title: "Leadership",
+  //   img_path: require("../images/category_img/Leadership.png"),
+  // },
+  // {
+  //   title: "Sales",
+  //   img_path: require("../images/category_img/Sales.png"),
+  // },
   // {
   //   title: "Communication",
   //   img_path: require("../images/category_img/Communication.png"),
   // },
-  {
-    title: "Health",
-    img_path: require("../images/category_img/Health.png"),
-  },
-  {
-    title: "Fitness",
-    img_path: require("../images/category_img/Fitness.png"),
-  },
+  // {
+  //   title: "Health",
+  //   img_path: require("../images/category_img/Health.png"),
+  // },
+  // {
+  //   title: "Fitness",
+  //   img_path: require("../images/category_img/Fitness.png"),
+  // },
   // {
   //   title: "Presentation skills",
-  //   img_path: require("../images/category_img/Presentation skills.png"),
+  //   img_path: require("../images/category_img/Presentationskills.png"),
   // },
   // {
   //   title: "Soft Skills",
-  //   img_path: require("../images/category_img/Soft Skills.png"),
+  //   img_path: require("../images/category_img/softskills.png"),
   // },
-  {
-    title: "Marketing",
-
-    img_path: require("../images/category_img/Marketing.png"),
-  },
-  {
-    title: "LGBTQ",
-    img_path: require("../images/category_img/LGBTQ.png"),
-  },
+  // {
+  //   title: "Marketing",
+  //   img_path: require("../images/category_img/Marketing.png"),
+  // },
+  // {
+  //   title: "LGBTQ",
+  //   img_path: require("../images/category_img/LGBTQ.png"),
+  // },
   // {
   //   title: "Oil and Gas",
-
-  //   img_path: require("../images/category_img/Oil and Gas.png"),
+  //   img_path: require("../images/category_img/OILGAS.png"),
   // },
-  {
-    title: "Manufacturing",
-    img_path: require("../images/category_img/Manufacturing.png"),
-  },
-  {
-    title: "Retail",
-    img_path: require("../images/category_img/Retail.png"),
-  },
+  // {
+  //   title: "Manufacturing",
+  //   img_path: require("../images/category_img/Manufacturing.png"),
+  // },
+  // {
+  //   title: "Retail",
+  //   img_path: require("../images/category_img/Retail.png"),
+  // },
   // {
   //   title: "Coaching",
   //   img_path: require("../images/category_img/Coaching.png"),
   // },
   // {
   //   title: "Information Technology",
-  //   img_path: require("../images/category_img/Information Technology.png"),
+  //   img_path: require("../images/category_img/IT.png"),
   // },
   // {
   //   title: "Education",
-
   //   img_path: require("../images/category_img/Education.png"),
   // },
-  {
-    title: "Motivation",
-    img_path: require("../images/category_img/Motivation.png"),
-  },
+  // {
+  //   title: "Motivation",
+  //   img_path: require("../images/category_img/Motivation.png"),
+  // },
   // {
   //   title: "Finance",
   //   img_path: require("../images/category_img/Finance.png"),
   // },
-  {
-    title: "Parenting",
-    img_path: require("../images/category_img/Parenting.png"),
-  },
-  {
-    title: "Innovation",
-    img_path: require("../images/category_img/Innovation.png"),
-  },
+  // {
+  //   title: "Parenting",
+  //   img_path: require("../images/category_img/Parenting.png"),
+  // },
+  // {
+  //   title: "Innovation",
+  //   img_path: require("../images/category_img/Innovation.png"),
+  // },
 ];
